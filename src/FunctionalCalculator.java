c21dac6d3c8f45c605e8fdeb783f6459479a88bc
Diana BUYINZA <buyinzadiana@gmail.com>
2026-02-20 10:31:03 +0200

    feat(calculator): add FunctionalCalculator with reusable calculate method using BiFunction

--- src/FunctionalCalculator.java
+++ src/FunctionalCalculator.java
@@ -1,2 +1,11 @@
+import java.util.function.BiFunction;
+
 public class FunctionalCalculator {
+
+    // This method takes 2 numbers and a "function" (operation)
+    public static Integer calculate(int num1, int num2,
+                                    BiFunction<Integer, Integer, Integer> operation) {
+
+        return operation.apply(num1, num2);
+    }
 }
